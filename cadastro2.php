ccb46c133144dc0b5d363bea89d354e9f38fabb8
Yasmim Letícia <yasmimleticia859@gmail.com>
2025-05-30 14:41:08 -0300

    Add files via upload

---
+++ cadastro2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirma cadastro</title>
+</head>
+<body>
+    <h1>Copnfirmação Cadastro</h1>
+    <h3>Dados Recebidos</h3>
+    <p>Nome: <?php echo $_POST["nome"]; ?></p>
+    <p>Sobrenome: <?php echo $_POST["sobrenome"]; /></p>
+</body>
+</html>
